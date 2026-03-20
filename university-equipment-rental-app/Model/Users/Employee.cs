dcc221f622be63c8b4891cde6dd6e690f393d354
MarcelKrawczyk <marcelkrawczyk04@gmail.com>
2026-03-20 11:42:00 +0100

    Add property for User subclasses

--- university-equipment-rental-app/Model/Users/Employee.cs
+++ university-equipment-rental-app/Model/Users/Employee.cs
@@ -3,10 +3,11 @@ namespace university_equipment_rental_app.Model.Users;
 public class Employee : User
 {
     public override int MaxActiveRentals => 5;
+    public string Department { get; set; }
 
-    public Employee(string firstName, string lastName) : base(firstName, lastName)
+    public Employee(string firstName, string lastName, string department) : base(firstName, lastName)
     {
-        
+        Department = department;
     }
     public override string GetUserType()
     {
--- university-equipment-rental-app/Model/Users/Student.cs
+++ university-equipment-rental-app/Model/Users/Student.cs
@@ -3,10 +3,11 @@ namespace university_equipment_rental_app.Model.Users;
 public class Student : User
 {
     public override int MaxActiveRentals => 2;
+    public string StudnetId { get; set; }
 
-    public Student(string firstName, string lastName) : base(firstName, lastName)
+    public Student(string firstName, string lastName, string studnetId) : base(firstName, lastName)
     {
-        
+        StudnetId = studnetId;
     }
     public override string GetUserType()
     {
